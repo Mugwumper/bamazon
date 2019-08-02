6b75e72aa11dd6a62ac8fec8e3c2d956db774c89
Thomas Lewis <TomPaul_67@yahoo.com>
2019-08-02 14:23:56 -0400

    - dead code removed  - read me updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bamazon
+
+
+## ISSUES
+There is a problem when reporting the invalid product id applied when performing a purchase. The code works but the display doesn't allow a new valid value to be entered.
+
+
+## NOTES
+If the error "ER_NOT_SUPPORTED_AUTH_MODE" occures run this in mySQL workbench:
+    ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mooselips123'
+
+Copy-paste to get git bash to the local folder:
+    cd  C:/data/Dropbox/Work/bootcamp/Homework/bamazon 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,10 @@
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var g_item_id_low = 0;
 var g_item_id_high = 0;
 
 var log = console.log;
-//  cd  C:/data/Dropbox/Work/bootcamp/Homework/bamazon 
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -24,7 +24,6 @@ function prompt_first() {
     log(" * * suggested that you widen your git bash window to allow each row to display on a single line. *  * ");
     prompt_main();
 }
-
 
 function prompt_main() {
   log('');
@@ -55,8 +54,6 @@ function prompt_main() {
       }
     });
 }
-
-
 
 var listProducts = function () {
   resetGlobalIDs();
@@ -132,168 +129,3 @@ function applyIDtoGlobals(id) {
   if (g_item_id_low > id) g_item_id_low = id;
   if (g_item_id_high < id) g_item_id_high = id;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var mysql = require("mysql");
-// var inquirer = require("inquirer");
-// var g_item_id_low = 0;
-// var g_item_id_high = 0;
-
-// //  cd  C:/data/Dropbox/Work/bootcamp/Homework/bamazon 
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-
-//     // Your port; if not 3306
-//     port: 3306,
-
-//     // Your username
-//     user: "root",
-
-//     // Your password
-//     password: "mooselips123",
-//     database: "bamazon"
-// });
-
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     prompt_first();
-// });
-
-// var log = console.log;
-
-// function prompt_first() {
-//     log("Welcome to Bamazon");
-//     log(" * * suggested that you widen your git bash window to allow each row to display on a single line. *  * ");
-//     listProducts();
-// }
-
-//   // item_id INT NOT NULL AUTO_INCREMENT,
-//   // product_name VARCHAR(45) NULL,
-//   // department_name VARCHAR(12) NULL,
-//   // price DECIMAL(10,2) NULL,
-//   // stock_quantity INT NULL,
-
-//   function listProducts() {
-//     log('lp');
-//     resetGlobalIDs();
-//     var query = "SELECT * from products";
-//     connection.query(query, function(err, res) {
-//       if (err) throw err;
-//       for (var i = 0; i < res.length; i++) {
-//         log("id: "    + res[i].item_id + 
-//         " || name: "  + res[i].product_name +
-//         " || dept: " + res[i].department_name +
-//         " || price: " + res[i].price +
-//         " || qty: "   + res[i].stock_quantity);
-//         applyIDtoGlobals(res[i].item_id);
-//       }
-//     });
-//     prompt_main();
-//   }
-
-//   function resetGlobalIDs() {
-//     g_item_id_low = 9999999;
-//     g_item_id_high = 0;
-//   }
-//   function applyIDtoGlobals(id) {
-//     if (g_item_id_low > id) g_item_id_low = id;
-//     if (g_item_id_high < id) g_item_id_high = id;
-//   }
-
-//   function prompt_purchase() {
-//       log('');
-//       var questions = [
-//         {
-//           type: "number",
-//           name: "item_id",
-//           message: "Enter 'id' for item you would like to purchase:",
-//           validate: validateItemId
-//         },{
-//           type: "number",
-//           name: "stock_quantity",
-//           message: "Quantity:"
-//         },
-//       ];
-//       function validateItemId(item_id){
-//         //log("item_id:" + item_id + " g_item_id_low:" + g_item_id_low + " g_item_id_high:" + g_item_id_high);
-//         //return ((item_id >= g_item_id_low) && (item_id <= g_item_id_high));
-//         if ((item_id >= g_item_id_low) && (item_id <= g_item_id_high)) {
-//           return true
-//         } else {
-//           return "Please enter a valid id";
-//         }
-//       }; 
-//       inquirer.prompt(questions, function(answers) {
-//         log(answers);
-
-//         if (purchaseValid(answers.item_id, answers.stock_quantity)) {
-//         }
-//       });
-//     }
-
-//   function prompt_purchaseX(){
-//     inquirer.prompt([
-//     {
-//       name: "ID",
-//       type: "input",
-//       message:"Please enter Item ID you like to purhcase.",
-//       filter:Number
-//     },
-//     {
-//       name:"Quantity",
-//       type:"input",
-//       message:"How many items do you wish to purchase?",
-//       filter:Number
-//     },
-
-//    ]).then(function(answers){
-//      var quantityNeeded = answers.Quantity;
-//      var IDrequested = answers.ID;
-//      purchaseOrder(IDrequested, quantityNeeded);
-//    });
-//   };
-
-//   function purchaseValid(item_id, stock_quantity) {
-//     var query = "SELECT * from products WHERE item_id = " + item_id ;
-//     connection.query(query, function(err, res) {
-//       if (err) throw err;
-//       for (var i = 0; i < res.length; i++) {
-//         log("id: " + res[i].item_id + 
-//         " || name: " + res[i].product_name +
-//         " || deptt: " + res[i].department_name +
-//         " || price: " + res[i].price +
-//         " || qty: " + res[i].stock_quantity);
-//       }
-//       return (res[i].stock_quantity > stock_quantity);
-//     });
-//   }
-
-
-//   function purchaseOrder(ID, amtNeeded){
-//     connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
-//       if(err){console.log(err)};
-//       if(amtNeeded <= res[0].stock_quantity){
-//         var totalCost = res[0].price * amtNeeded;
-//         console.log("Good news your order is in stock!");
-//         console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
-
-//         connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + " WHERE item_id = " + ID);
-//       } else{
-//         console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
-//       };
-//      // prompt_main();
-//     });
-//   };
-
