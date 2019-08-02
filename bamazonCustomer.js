497b7a6b75106ef7cbb4f9db5da659e5561cb89b
Thomas Lewis <TomPaul_67@yahoo.com>
2019-08-02 13:58:22 -0400

    min functionality in place

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer"); 
+var inquirer = require("inquirer");
 var g_item_id_low = 0;
 var g_item_id_high = 0;
 
@@ -7,84 +7,98 @@ var log = console.log;
 //  cd  C:/data/Dropbox/Work/bootcamp/Homework/bamazon 
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mooselips123",
-    database: "bamazon"
-});
-  
-
-connection.connect(function(err){
-	if(err)throw err;
-	console.log("connected as id" + connection.threadId);
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mooselips123",
+  database: "bamazon"
 });
 
-var displayProducts = function(){
-  resetGlobalIDs();
-	var query = "Select * FROM products";
-	connection.query(query, function(err, res){
-    if(err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        log("id: "    + res[i].item_id + 
-        " || name: "  + res[i].product_name +
-        " || deptt: " + res[i].department_name +
-        " || price: " + res[i].price +
-        " || qty: "   + res[i].stock_quantity);
-        applyIDtoGlobals(res[i].item_id);
-      }
-		purchasePrompt();
-	});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
+});
+
+function prompt_first() {
+    log("Welcome to Bamazon");
+    log(" * * suggested that you widen your git bash window to allow each row to display on a single line. *  * ");
+    listProducts();
 }
 
-function purchasePrompt(){
-	inquirer.prompt([
-	{
-		name: "ID",
-		type: "input",
-		message:"Please enter Item ID you like to purhcase.",
-		filter:Number
-	},
-	{
-		name:"Quantity",
-		type:"input",
-		message:"How many items do you wish to purchase?",
-		filter:Number
-	},
+var listProducts = function () {
+  resetGlobalIDs();
+  var query = "Select * FROM products";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      log("id: " + res[i].item_id +
+        " || name: " + res[i].product_name +
+        " || dept: " + res[i].department_name +
+        " || price: " + res[i].price +
+        " || qty: " + res[i].stock_quantity);
+      applyIDtoGlobals(res[i].item_id);
+    }
+    purchasePrompt();
+  });
+}
 
- ]).then(function(answers){
- 	var quantityNeeded = answers.Quantity;
- 	var IDrequested = answers.ID;
- 	purchaseOrder(IDrequested, quantityNeeded);
- });
+function purchasePrompt() {
+  inquirer.prompt([
+    {
+      name: "ID",
+      type: "input",
+      message: "Please enter Item ID you like to purhcase.",
+      validate: validateItemId,
+      filter: Number
+    },
+    {
+      name: "Quantity",
+      type: "input",
+      message: "How many items do you wish to purchase?",
+      filter: Number
+    },
+
+  ]).then(function (answers) {
+    var quantityNeeded = answers.Quantity;
+    var IDrequested = answers.ID;
+    purchaseOrder(IDrequested, quantityNeeded);
+  });
+  function validateItemId(item_id){
+    //log("item_id:" + item_id + " g_item_id_low:" + g_item_id_low + " g_item_id_high:" + g_item_id_high);
+    //return ((item_id >= g_item_id_low) && (item_id <= g_item_id_high));
+      if ((item_id >= g_item_id_low) && (item_id <= g_item_id_high)) {
+        return true
+      } else {
+        return "Please enter a valid id";
+      }
+    }; 
 };
 
-function purchaseOrder(ID, amtNeeded){
-	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
-		if(err){console.log(err)};
-		if(amtNeeded <= res[0].stock_quantity){
-			var totalCost = res[0].price * amtNeeded;
-			console.log("Good news your order is in stock!");
-			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
-
-			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + " WHERE item_id = " + ID);
-		} else{
-			console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
-		};
-		displayProducts();
-	});
+function purchaseOrder(ID, amtNeeded) {
+  connection.query('Select * FROM products WHERE item_id = ' + ID, function (err, res) {
+    if (err) { console.log(err) };
+    if (amtNeeded <= res[0].stock_quantity) {
+      var totalCost = res[0].price * amtNeeded;
+      console.log("Good news your order is in stock!");
+      console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + " is $" + totalCost + " Thank you!");
+      connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + " WHERE item_id = " + ID);
+    } else {
+      console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+    };
+    listProducts();
+  });
 };
 
-displayProducts(); 
+prompt_first();
 
-  function resetGlobalIDs() {
-    g_item_id_low = 9999999;
-    g_item_id_high = 0;
-  }
-  function applyIDtoGlobals(id) {
-    if (g_item_id_low > id) g_item_id_low = id;
-    if (g_item_id_high < id) g_item_id_high = id;
-  }
+function resetGlobalIDs() {
+  g_item_id_low = 9999999;
+  g_item_id_high = 0;
+}
+function applyIDtoGlobals(id) {
+  if (g_item_id_low > id) g_item_id_low = id;
+  if (g_item_id_high < id) g_item_id_high = id;
+}
 
 
 
@@ -107,67 +121,67 @@ displayProducts();
 
 // var connection = mysql.createConnection({
 //     host: "localhost",
-  
+
 //     // Your port; if not 3306
 //     port: 3306,
-  
+
 //     // Your username
 //     user: "root",
-  
+
 //     // Your password
 //     password: "mooselips123",
 //     database: "bamazon"
 // });
-  
+
 // connection.connect(function(err) {
 //     if (err) throw err;
 //     prompt_first();
 // });
 
 // var log = console.log;
-  
+
 // function prompt_first() {
 //     log("Welcome to Bamazon");
 //     log(" * * suggested that you widen your git bash window to allow each row to display on a single line. *  * ");
 //     listProducts();
 // }
 
-// function prompt_main() {
-//     log('m');
-//     inquirer
-//       .prompt({
-//         name: "action",
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: [
-//           "List Products",
-//           "Make a Purchase",
-//           "exit"
-//         ]
-//       })
-//       .then(function(answer) {
-//         switch (answer.action) {
-//         case "List Products":
-//           listProducts();
-//           break;
-  
-//         case "Make a Purchase":
-//           prompt_purchase();
-//           break;
-  
-//         case "exit":
-//           connection.end();
-//           break;
-//         }
-//       });
-// }
+function prompt_main() {
+    log('m');
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "List Products",
+          "Make a Purchase",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "List Products":
+          listProducts();
+          break;
+
+        case "Make a Purchase":
+          prompt_purchase();
+          break;
+
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+}
 
 //   // item_id INT NOT NULL AUTO_INCREMENT,
 //   // product_name VARCHAR(45) NULL,
 //   // department_name VARCHAR(12) NULL,
 //   // price DECIMAL(10,2) NULL,
 //   // stock_quantity INT NULL,
-  
+
 //   function listProducts() {
 //     log('lp');
 //     resetGlobalIDs();
@@ -177,7 +191,7 @@ displayProducts();
 //       for (var i = 0; i < res.length; i++) {
 //         log("id: "    + res[i].item_id + 
 //         " || name: "  + res[i].product_name +
-//         " || deptt: " + res[i].department_name +
+//         " || dept: " + res[i].department_name +
 //         " || price: " + res[i].price +
 //         " || qty: "   + res[i].stock_quantity);
 //         applyIDtoGlobals(res[i].item_id);
@@ -220,7 +234,7 @@ displayProducts();
 //       }; 
 //       inquirer.prompt(questions, function(answers) {
 //         log(answers);
-      
+
 //         if (purchaseValid(answers.item_id, answers.stock_quantity)) {
 //         }
 //       });
@@ -240,7 +254,7 @@ displayProducts();
 //       message:"How many items do you wish to purchase?",
 //       filter:Number
 //     },
-  
+
 //    ]).then(function(answers){
 //      var quantityNeeded = answers.Quantity;
 //      var IDrequested = answers.ID;
@@ -271,7 +285,7 @@ displayProducts();
 //         var totalCost = res[0].price * amtNeeded;
 //         console.log("Good news your order is in stock!");
 //         console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
-  
+
 //         connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + " WHERE item_id = " + ID);
 //       } else{
 //         console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
